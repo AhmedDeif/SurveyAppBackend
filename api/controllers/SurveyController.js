48d3f8e669ef6d07cff62f59f9f4616ac8b21f34
Doola <ahmed.adel.abodeif@gmail.com>
2017-11-20 17:37:44 +0200

    controller skeleton

---
+++ api/controllers/SurveyController.js
@@ -0,0 +1,43 @@
+module.exports = {
+
+    // Allows the admin to a new question to survey
+    addQuestion : function(req, res) {
+
+    },
+
+    // Allows the admin to remove a question from survey, set as inactive
+    removeQuestion: function(req, res) {
+
+    }
+
+    // Allows the admin to add an answer to a question in survey
+    addAnswer : function(req, res) {
+
+    },
+
+    // Allows the admin to remove an answer to a question in survey
+    removeAnswer : function(req, res) {
+
+    },
+
+    // Allows the admin to view a specific response (answers to all questions of a specific user)
+    viewResponse : function(req, res) {
+
+    },
+
+    // Allows the admin to set a question as flagged. Flagged questions should return alerts
+    // when a certain answer is selected.
+    flagQuestion : function(req, res) {
+
+    },
+
+    // Allows the admin to see the average answers of all questions
+    viewAverageAnswers : function(req, res) {
+
+    },
+
+    // Allows the user to see the percentage of each answer of a specific question
+    viewQuestionAnswers : function(req, res) {
+
+    },
+}
